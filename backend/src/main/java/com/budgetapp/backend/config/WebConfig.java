10ae0ffe0f8358639a030f65e8b5634a69530d54
Cooper Rachow <cooper.rachow@icloud.com>
2025-05-30 20:14:02 -0500

    Update webconfig

--- backend/src/main/java/com/budgetapp/backend/config/WebConfig.java
+++ backend/src/main/java/com/budgetapp/backend/config/WebConfig.java
@@ -1,13 +1,13 @@
-package com.budgetapp.backend.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig {
-
+//package com.budgetapp.backend.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig {
+//
 //    @Bean
 //    public WebMvcConfigurer corsConfigurer() {
 //        return new WebMvcConfigurer() {
@@ -25,4 +25,4 @@ public class WebConfig {
 //            }
 //        };
 //    }
-}
+//}
