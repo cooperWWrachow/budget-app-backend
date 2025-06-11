4961783c16ee21ad5e746687c087ec4fd39252bb
Cooper Rachow <cooper.rachow@icloud.com>
2025-06-10 23:34:46 -0500

    Update hello service for testing

--- backend/src/main/java/com/budgetapp/backend/hello/HelloService.java
+++ backend/src/main/java/com/budgetapp/backend/hello/HelloService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class HelloService {
     public String getGreeting() {
-        return "Hello World! pt2";
+        return "Hello World!";
     }
 }
